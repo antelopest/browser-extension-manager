fb2d3d35a0a39bdbf151dab36f0be1aa4810eb32
antelopest <antelopest@gmail.com>
2025-07-10 14:26:01 +0700

    main changes

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
-import {fileURLToPath} from 'url';
-import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import { fileURLToPath } from 'url';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
@@ -11,79 +11,79 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    entry: './src/main.js',
+  entry: './src/main.js',
 
-    output: {
-        filename: 'bundle.[contenthash].js',
-        path: path.join(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/browser-extension-manager'
-    },
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.join(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/browser-extension-manager/'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: ['autoprefixer']
-                            }
-                        }
-                    },
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-                options: {
-                    sources: false
-                }
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer']
+              }
             }
+          },
+          'sass-loader'
         ]
-    },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          sources: false
+        }
+      }
+    ]
+  },
 
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: '404.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'main.[contenthash].css'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/assets'),
-                    to: 'assets',
-                    noErrorOnMissing: true
-                }
-            ]
-        })
-    ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: '404.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.[contenthash].css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: 'assets',
+          noErrorOnMissing: true
+        }
+      ]
+    })
+  ],
 
-    devServer: {
-        static: './dist',
-        port: 3000,
-        open: true,
-        hot: true,
-        liveReload: true,
-        watchFiles: ['./src/**/*']
-    },
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    hot: true,
+    liveReload: true,
+    watchFiles: ['./src/**/*']
+  },
 
-    resolve: {
-        extensions: ['.js']
-    },
+  resolve: {
+    extensions: ['.js']
+  },
 
-    mode: 'development'
+  mode: 'development'
 };
